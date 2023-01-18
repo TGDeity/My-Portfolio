4f9e238fd59351fb338680e2f80744d989311f33
tgatech <tolga.1dr@gmail.com>
2023-01-18 18:22:59 +0300

    TGATECH

--- Staj-takip/resources/views/front/include/body/logged/student/internship_info.blade.php
+++ Staj-takip/resources/views/front/include/body/logged/student/internship_info.blade.php
@@ -58,65 +58,160 @@
                         <div class="col-md-6 col-lg-4">
                             <div class="project-card-no-image">
                                 <h3>{{ $days }}.Gün</h3>
-                                <h4>Başarıyla tamamlandı</h4><a class="btn btn-primary btn-lg text-lowercase" role="button"
-                                    data-bs-target={{ $daysCounterForDataTarget }} data-bs-toggle="modal"
-                                    style="background: transparent;color: var(--bs-blue);">İncele</a>
+                                @if ($getVal->description_stu != null && $getVal->description_emp != null)
+                                    <h4>Başarıyla tamamlandı</h4>
+                                @else
+                                    <h4>Başarıyla tamamlanamadı</h4>
+                                @endif
 
-                                <div class="modal fade" role="dialog" tabindex={{ $modalTablIndex }}
-                                    id={{ $daysCounterForModelId }}>
-                                    <div class="modal-dialog modal-xl modal-body-xl" role="document">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h4>{{ $days }}.Gün</h4><button type="button" class="btn-close"
-                                                    data-bs-dismiss="modal" aria-label="Close"></button>
-                                            </div>
+                                @if ($getVal->description_stu == null)
+                                    <a class="btn btn-primary btn-lg text-lowercase" role="button"
+                                        data-bs-target={{ $daysCounterForDataTarget }} data-bs-toggle="modal"
+                                        style="background: transparent;color: var(--bs-blue);">İncele
+                                    </a>
+                                    <div class="modal fade" role="dialog" tabindex={{ $modalTablIndex }}
+                                        id={{ $daysCounterForModelId }}>
+                                        <div class="modal-dialog modal-xl modal-body-xl" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h4>{{ $days }}.Gün</h4><button type="button" class="btn-close"
+                                                        data-bs-dismiss="modal" aria-label="Close"></button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <div class="container-fluid ">
+                                                        <div class="row ">
+                                                            <div class="col-12">
 
-                                            <div class="modal-body">
-                                                {{--            <p class="text-center text-muted">{{ $getVal->description_stu }} </p>
-                                                <p class="text-center text-muted">{{ $getVal->description_emp }} </p> --}}
-                                                <div class="container-fluid ">
-                                                    <div class="row ">
-                                                        <div class="col-12">
-                                                            <form action="" class="form">
+                                                                <form action="" class="form">
+                                                                    <div class="form-group">
+                                                                        <label for="description">Bugün neler yaptın?</label>
+                                                                        <textarea required type="text" class="form-control" id="decsription" name="description" rows="10" cols="10"
+                                                                            placeholder="Bugün yaptıklarım..."></textarea>
+                                                                        <small id="descriptionHelp"
+                                                                            class="form-text text-muted">Lütfen gün içinde
+                                                                            yaptığınız işlemleri kutucuğa yazınız.</small>
 
-                                                                <div class="form-group">
-                                                                    <label for="description">Bugün neler yaptın?</label>
-                                                                    <textarea required type="text" class="form-control" id="decsription" name="description" rows="10" cols="10"
-                                                                        placeholder="Bugün yaptıklarım..."></textarea>
-                                                                    <small id="descriptionHelp"
-                                                                        class="form-text text-muted">Lütfen gün içinde
-                                                                        yaptığınız işlemleri kutucuğa yazınız.</small>
-
-                                                                </div>
-
-                                                                <div class="row mt-5">
-                                                                    <div class="col-sm-7 col-md-10">
-                                                                        <div class="form-check">
-                                                                            <input required class="form-check-input"
-                                                                                type="checkbox" value="onay"
-                                                                                id="onay">
-                                                                            <label class="form-check-label" for="onay">
-                                                                                Yukarıda yazıklarımı onaylıyorum.
-                                                                            </label>
-                                                                        </div>
                                                                     </div>
-                                                                    <div class="col-sm-5 col-md-2"><button
-                                                                            class="btn btn-primary"
-                                                                            type="submit">Gönder</button></div>
+                                                                    <div class="row mt-5">
+                                                                        <div class="col-sm-7 col-md-10">
+                                                                            <div class="form-check">
+                                                                                <input required class="form-check-input"
+                                                                                    type="checkbox" value="onay"
+                                                                                    id="onay">
+                                                                                <label class="form-check-label"
+                                                                                    for="onay">
+                                                                                    Yukarıda yazıklarımı onaylıyorum.
+                                                                                </label>
+                                                                            </div>
+                                                                        </div>
+                                                                        <div class="col-sm-5 col-md-2"><button
+                                                                                class="btn btn-primary"
+                                                                                type="submit">Gönder</button></div>
 
-                                                                </div>
-                                                            </form>
+                                                                    </div>
+                                                                </form>
+                                                            </div>
                                                         </div>
                                                     </div>
-                                                </div>
-                                                <div class="modal-footer">
-                                                    <button class="btn btn-light" type="button"
-                                                        data-bs-dismiss="modal">Kapat</button>
+                                                    <div class="modal-footer">
+                                                        <button class="btn btn-light" type="button"
+                                                            data-bs-dismiss="modal">Kapat</button>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
+                                @else
+                                    {{-- eğer input girişi sağlanmamışsa student yada emp tarafından calısacak. --}}
+                                    <a class="btn btn-primary btn-lg text-lowercase" role="button"
+                                        data-bs-target={{ $daysCounterForDataTarget }} data-bs-toggle="modal"
+                                        style="background: transparent;color: var(--bs-blue);">İncele
+                                    </a>
+                                    <div class="modal fade" role="dialog" tabindex={{ $modalTablIndex }}
+                                        id={{ $daysCounterForModelId }}>
+                                        <div class="modal-dialog modal-xl modal-body-xl" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h4>{{ $days }}.Gün</h4>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                        aria-label="Close">
+                                                    </button>
+                                                </div>
+
+                                                <div class="modal-body">
+                                                    <div class="container-fluid ">
+                                                        <div class="row mb-5">
+                                                            <div class="col-12">
+                                                                @if ($getVal->description_stu == null)
+                                                                    <form action="" class="form">
+                                                                        <div class="form-group">
+                                                                            <label for="description">Bugün neler
+                                                                                yaptın?</label>
+                                                                            <textarea required type="text" class="form-control" id="decsription" name="description" rows="10"
+                                                                                cols="10" placeholder="Bugün yaptıklarım..."></textarea>
+                                                                            <small id="descriptionHelp"
+                                                                                class="form-text text-muted">Lütfen gün
+                                                                                içinde
+                                                                                yaptığınız işlemleri kutucuğa
+                                                                                yazınız.</small>
+
+                                                                        </div>
+                                                                        <div class="row mt-5">
+                                                                            <div class="col-sm-7 col-md-10">
+                                                                                <div class="form-check">
+                                                                                    <input required
+                                                                                        class="form-check-input"
+                                                                                        type="checkbox" value="onay"
+                                                                                        id="onay">
+                                                                                    <label class="form-check-label"
+                                                                                        for="onay">
+                                                                                        Yukarıda yazıklarımı onaylıyorum.
+                                                                                    </label>
+                                                                                </div>
+                                                                            </div>
+                                                                            <div class="col-sm-5 col-md-2"><button
+                                                                                    class="btn btn-primary"
+                                                                                    type="submit">Gönder</button></div>
+
+                                                                        </div>
+                                                                    </form>
+                                                                @else
+                                                                    <div class="row">
+                                                                        <div class="col-lg-6 col-sm-12 border">
+                                                                            <label
+                                                                                class="col-12 mt-1 fw-bold text-center">ÖĞRENCİ
+                                                                                NOTU</label>
+                                                                            <hr>
+                                                                            <div class="col-12 pt-2 m-2">
+                                                                                {{ $getVal->description_stu }}</div>
+                                                                        </div>
+
+                                                                        <div class="col-lg-6 col-sm-12 border">
+                                                                            <label
+                                                                                class="col-12 mt-1 fw-bold text-center ">İŞVEREN
+                                                                                NOTU
+                                                                            </label>
+                                                                            <hr>
+                                                                            <div class="col-12 pt-2 m-2">
+                                                                                {{ $getVal->description_emp == null ? 'Not girişi bekleniyor' : $getVal->description_emp }}
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                @endif
+                                                            </div>
+                                                        </div>
+
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button class="btn btn-light" type="button"
+                                                            data-bs-dismiss="modal">Kapat</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                @endif
+
                             </div>
                         </div>
                     @endforeach
